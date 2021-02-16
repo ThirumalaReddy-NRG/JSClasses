2f246bf8c92425cd05693803df2fbb3aa6014e68
Thirumala Reddy <nrgtreddy@gmail.com>
2021-02-16 20:54:30 +0530

    xxx

---
+++ main.js
@@ -0,0 +1,6 @@
+const myHeading = document.querySelector('h1');
+
+myHeading.textContent = 'Hello world...........!';
+
+alert(myHeading.textContent);
+alert("Test main.js by importing through VS Code.");
